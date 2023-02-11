2bb872e141d26841fd6aae709e3aa15ad30ab51c
Pushpak Jain <pushpakjain628@gmail.com>
2023-02-11 11:28:03 +0530

    initial commit before starting stats

---
+++ src/components/Expense/ExpenseItem.js
@@ -0,0 +1,26 @@
+import "./styles/ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from '../UI/Card';
+import React from 'react';
+
+const ExpensionItem = (props) => {
+
+  let title = props.title;
+
+  const clickHandler = () => {
+    title= "Updated";
+  }
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount} </div>
+      </div>
+
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpensionItem;
---
+++ src/components/Expense/Expenses.js
@@ -0,0 +1,28 @@
+import Card from "../UI/Card";
+import ExpensionItem from "./ExpenseItem";
+import './styles/Expenses.css'
+import React from 'react';
+
+
+const Expense = (props) => {
+   let expenses= props.expenses
+  return (
+    <Card className="expenses">
+      <ExpensionItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+        id={expenses[0].id} 
+      ></ExpensionItem>
+
+      <ExpensionItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        id={expenses[1].id}
+      ></ExpensionItem>
+    </Card>
+  );
+}
+
+export default Expense;
